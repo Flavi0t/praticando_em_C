9b851c59d68b31a2e93a9020681570c05a05257f
Flávio Alberto <128648351+Flavi0t@users.noreply.github.com>
2023-05-29 19:50:40 -0300

    Add files via upload

---
+++ usuario.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m, j;
+    printf("Por favor, insira os tres numeros(inteiros) desejados:\n");
+    scanf("%d%d%d", &n, &m, &j);
+    printf("Soma dos numeros inseridos: %d\n", (n+m+j));
+    return 0;
+}
